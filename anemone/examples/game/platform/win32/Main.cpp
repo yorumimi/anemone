2243b36db6b9446a05ad01fd1f7b2b4045d7817a
yorumimi <yorumimi18@gmail.com>
2023-01-24 15:42:22 +0900

    add main.cpp

---
+++ anemone/examples/game/platform/win32/Main.cpp
@@ -0,0 +1,9 @@
+#include <Windows.h>
+
+#define ENTRY_POINT \
+  int APIENTRY WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ int nCmdShow)
+
+ENTRY_POINT
+{
+  return 0;
+}
